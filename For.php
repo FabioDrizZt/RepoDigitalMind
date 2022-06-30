55dad561818c40c2ece1121621471e2336569f8b
Fabio <ing.fabio.arg@gmail.com>
2022-06-30 19:06:12 -0300

    [FIX] Quite algunas lineas de comentario

--- For.php
+++ For.php
@@ -12,13 +12,6 @@ for (expr1; expr2; expr3)
 {
 //código
 }
-
-La primera expresión se ejecuta una vez al principio del bucle.
-La mayor parte de las veces se trata de un valor puesto a cero.
-La segunda expresión es una declaración condicional, que se evalúa al principio del bucle en cada iteración.
-Si la expresión es verdadera, se ejecuta el código dentro de los paréntesis.
-La tercera expresión se ejecuta al final de cada una de las iteraciones del bucle.
-
  */
 
 /* $i = 0;
